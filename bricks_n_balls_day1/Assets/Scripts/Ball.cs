b46e3e7eb16f906a489b0455504ef9d1f9de4fa3
TakedaNaomichi <takeda.naomichi@donuts.ne.jp>
2023-12-07 10:27:51 +0900

    ボールの実装

---
+++ bricks_n_balls_day1/Assets/Scripts/Ball.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D rigidbody;
+
+    private float speed = 2000.0f;
+    private Vector3 velocity = Vector3.zero;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        rigidbody.velocity = velocity * speed * Time.deltaTime;
+    }
+
+    public void SetVelocity(Vector3 newVelocity)
+    {
+        velocity = newVelocity;
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Wall") || other.gameObject.CompareTag("Block"))
+        {
+            velocity = Vector3.Reflect(velocity, other.contacts[0].normal).normalized;
+        }
+    }
+}
